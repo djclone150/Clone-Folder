dae9d3f6b3d9383d43c60ca2c50aa57d83dd0e47
Nicolai Zammit <a100944@mcast.org.mt>
2017-12-11 10:45:20 +0100

    The ball shoots in a random direction of left and right, the point system is work in progress but the lose collider still needs to be fixed to count score.

--- Ping Pong Assignment/Assets/Scripts/Ball.cs
+++ Ping Pong Assignment/Assets/Scripts/Ball.cs
@@ -2,27 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Ball : MonoBehaviour {
+    private bool hasStarted = false;
 
-	// Use this for initialization
-	void Start () {
-        int rand = Random.Range(0, 2);
-
-        if (rand == 0)
-        {
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(-6f, 0f);
-        }
-        else if (rand == 1)
-        {
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(6f, 0f);
-        }
-		
+    // Use this for initialization
+    void Start () {
+        
+           
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("pressed");
+            hasStarted = true;
+
+        }
+
+        int rand = Random.Range(0, 2);
+        if (hasStarted == true)
+        {
+            hasStarted = false;
+            Debug.Log("Pressed left click");
+            if (rand == 0)
+            {
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(-6f, -5f);
+            }
+            else if (rand == 1)
+            {
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(6f, 5f);
+            }
+        }
+
+    }
 
     private void OnCollisionEnter2D(Collision2D hit)
     {
--- Ping Pong Assignment/Assets/Scripts/LoseColliderr.cs
+++ Ping Pong Assignment/Assets/Scripts/LoseColliderr.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LooseCollider : MonoBehaviour {
+public class LoseColliderr : MonoBehaviour
+{
+
     public LevelManager levelManager;
-    static int playerScoreLeft = 0;
+    static int playerScoreRight = 0;
 
 
     void OnTriggerEnter2D(Collider2D collider)//this is triggered when a trigger is set
     {
         print("Trigger");
-        playerScoreLeft += 1;
-        print(playerScoreLeft);
+        playerScoreRight += 1;
+        print(playerScoreRight);
         levelManager.LoadLevel("Lose"); //loads Win Scene ball enteres trigger
         
     }

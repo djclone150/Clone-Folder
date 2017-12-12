89ace625ff1859622b5b6e08927cf7ca10ae2b8a
djclone150 <snail150@hotmail.com>
2017-12-12 19:28:06 +0100

    Finished level 2, just need to add static obstacles and re adjust the score count requirements to go to next level

---
+++ Ping Pong Assignment/Assets/Scripts/LooseCollider2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LooseCollider2 : MonoBehaviour
+{
+    private LevelManager levelManager;
+    static int playerScoreRight = 0;
+    public Text scoreRight;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //print("Collision with: " + collision.gameObject.name);
+    }
+
+    //works when Collider is Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("Triggered with: " + collision.gameObject.name);
+        playerScoreRight += 1;
+        print("Right Player Score: " + playerScoreRight);
+        if (playerScoreRight == 3)
+        {
+            print("Right player won this round!");
+            levelManager.LoadLevel("Level3");
+        }
+        else
+            levelManager.LoadLevel("Level2");
+
+
+    }
+
+    void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+    }
+
+    void Update()
+    {
+        scoreRight.text = playerScoreRight.ToString();
+    }
+
+
+}
+
---
+++ Ping Pong Assignment/Assets/Scripts/LoseCollider2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseCollider2 : MonoBehaviour
+{
+    private LevelManager myLevelManager;
+    static int playerScoreLeft = 0;
+    public Text scoreLeft;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //print("Collision with: " + collision.gameObject.name);
+    }
+
+    //works when Collider is Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("Triggered with: " + collision.gameObject.name);
+        playerScoreLeft += 1;
+        print("Left Player Score: " + playerScoreLeft);
+        if (playerScoreLeft == 3)
+        {
+            print("Left Player has won this round!");
+            myLevelManager.LoadLevel("Level3");
+        }
+        else
+            myLevelManager.LoadLevel("Level2");
+
+
+    }
+
+    void Start()
+    {
+        myLevelManager = GameObject.FindObjectOfType<LevelManager>();
+
+    }
+
+    void Update()
+    {
+        scoreLeft.text = playerScoreLeft.ToString();
+    }
+
+}
